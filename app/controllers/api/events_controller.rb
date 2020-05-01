3aa739303d073692e4f1780f42765bf50581a08f
Dan Clair <danclair@me.com>
2020-04-30 20:53:32 -0500

    Add RESTful api Events routes, add index & create actions, add events views

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,2 +1,22 @@
 class Api::EventsController < ApplicationController
+  def index
+    @events = Event.all
+    render "index.json.jb"
+  end
+
+  def create
+    @event = Event.new(
+      title: params[:title],
+      description: params[:description],
+      date: params[:date],
+      time: params[:time],
+      location: params[:location],
+      # user_id: current_user,
+    )
+    if @event.save
+      render "show.json.jb"
+    else
+      render json: { errors: @event.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # EXAMPLE HTML ROUTE
-  # get "/photos" => "photos#index"
+  get "/events" => "events#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/events" => "events#index"
+    post "/events" => "events#create"
+    get "/events/:id" => "events#show"
+    patch "/events/:id" => "events#update"
+    delete "/events/:id" => "events#destroy"
+  end
 end
