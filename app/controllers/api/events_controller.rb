2b7e8b813abdfe7bd0aada8140008b885c394b3b
Dan Clair <danclair@me.com>
2020-04-30 21:27:55 -0500

    Add update action

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -11,8 +11,27 @@ class Api::EventsController < ApplicationController
       date: params[:date],
       time: params[:time],
       location: params[:location],
-      # user_id: current_user,
+      user_id: params[:user_id],
     )
+    if @event.save
+      render "show.json.jb"
+    else
+      render json: { errors: @event.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @event = Event.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @event = Event.find_by(id: params[:id])
+    @event.title = params[:title] || @event.title
+    @event.description = params[:description] || @event.description
+    @event.date = params[:date] || @event.date
+    @event.time = params[:time] || @event.time
+    @event.location = params[:location] || @event.location
     if @event.save
       render "show.json.jb"
     else
