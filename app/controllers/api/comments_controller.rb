04baf7d44fac878e3f962bab7ef63266a9261057
Dan Clair <danclair@me.com>
2020-05-03 16:25:19 -0500

    Add comments create/show/update/destroy methods

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -5,5 +5,36 @@ class Api::CommentsController < ApplicationController
   end
 
   def create
+    @comment = Comment.new(
+      text: params[:text],
+      event_id: params[:event_id],
+      user_id: params[:user_id],
+    )
+    if @comment.save
+      render "show.json.jb"
+    else
+      render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @comment = Comment.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @comment = Comment.find_by(id: params[:id])
+    @comment.text = params[:text] || @comment.text
+    if @comment.save
+      render "show.json.jb"
+    else
+      render json: { errors: @comment.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    comment = Comment.find_by(id: params[:id])
+    comment.destroy
+    render json: { message: "Comment successfully destroyed" }
   end
 end
